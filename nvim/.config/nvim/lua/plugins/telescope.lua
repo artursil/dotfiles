be10960dcdaf9e46789ab21aab6f0f5bf04f4cfc
artursil <artur.silicki@gmail.com>
2025-02-15 20:07:21 +0100

    ripgrep for live_grep in telecope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -18,6 +18,17 @@ return {
       vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
       vim.keymap.set("n", "<leader>fo", builtin.oldfiles, {})
+      vim.keymap.set("n", "<leader>fa", function()
+        local python_site_packages = vim.fn.systemlist("python3 -c 'import site; print(site.getsitepackages()[0])'")[1]
+        require("telescope.builtin").live_grep({
+          -- additional_args = function()
+          --   return {
+          --     "--regexp", '"(def|class) '  -- Search for "def " or "class " inside JSON strings
+          --   }
+          -- end,
+          search_dirs = { ".", python_site_packages }
+        })
+      end, { noremap = true, silent = true })
 
       require("telescope").load_extension("ui-select")
     end,
