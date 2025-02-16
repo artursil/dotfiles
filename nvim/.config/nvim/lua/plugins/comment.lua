cd7d4f55f076b29e6cf92ac74312d2405c239f1b
artursil <artur.silicki@gmail.com>
2025-02-16 19:11:20 +0100

    fixing commenting

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -1,6 +1,28 @@
 return {
     'numToStr/Comment.nvim',
     opts = {
-        -- add any options here
+        ---Add a space b/w comment and the line
+        padding = true,
+        ---Whether the cursor should stay at its position
+        sticky = true,
+        toggler = {
+        ---Lines to be ignored while (un)comment
+            ---Line-comment toggle keymap
+            line = '<C-/>',  -- Change `gcc` to `<C-/>`
+            ---Block-comment toggle keymap
+            block = '<C-?>', -- Change `gbc` to `<C-?>` (or another key if `<C-/>` fails)
+        },
+        ---LHS of operator-pending mappings in NORMAL and VISUAL mode
+        opleader = {
+            ---Line-comment keymap
+            line = '<C-/>',  -- Change `gc` to `<C-/>`
+            ---Block-comment keymap
+            block = '<C-?>', -- Change `gb` to `<C-?>`
+        },
+        ---Enable keybindings
+        mappings = {
+            basic = true, -- Enables the operator-pending and toggler mappings
+            extra = true, -- Enables extra mappings
+        }
     }
 }
