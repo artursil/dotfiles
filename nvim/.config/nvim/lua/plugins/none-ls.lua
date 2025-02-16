e529179636a000fdf65903b24aed40767595c451
artursil <artur.silicki@gmail.com>
2025-02-16 17:51:42 +0100

    formatted

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,36 +1,45 @@
+-- Format on save and linters
 return {
-    "nvimtools/none-ls.nvim",
-    enabled = true,
-    dependencies = {
-        'nvimtools/none-ls-extras.nvim',
-        'jayp0521/mason-null-ls.nvim', -- ensure dependencies are installed
-    },
-    config = function()
-        local null_ls = require("null-ls")
-        local formatting = null_ls.builtins.formatting   -- to setup formatters
-        local diagnostics = null_ls.builtins.diagnostics -- to setup linters
-        -- list of formatters & linters for mason to install
-        require('mason-null-ls').setup {
-            ensure_installed = {
-                'checkmake', -- grammar
-                'prettier',  -- ts/js formatter
-                'stylua',    -- lua formatter
-                'shfmt',     -- bash
-                'ruff',
-            },
-            -- auto-install configured formatters & linters (with null-ls)
-            automatic_installation = true,
-        }
-        local sources = {
-            diagnostics.checkmake,
-            formatting.prettier.with { filetypes = { 'html', 'json', 'yaml', 'markdown' } },
-            formatting.stylua,
-            formatting.shfmt.with { args = { '-i', '4' } },
-            formatting.terraform_fmt,
-            require('none-ls.formatting.ruff').with { extra_args = { '--extend-select', 'I' } }, --sorting imports
-            require('none-ls.formatting.ruff_format'),
-        }
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+		"jayp0521/mason-null-ls.nvim", -- ensure dependencies are installed
+	},
+	config = function()
+		local null_ls = require("null-ls")
+		local formatting = null_ls.builtins.formatting -- to setup formatters
+		local diagnostics = null_ls.builtins.diagnostics -- to setup linters
 
+		-- list of formatters & linters for mason to install
+		require("mason-null-ls").setup({
+			ensure_installed = {
+				"checkmake",
+				"prettier", -- ts/js formatter
+				"stylua", -- lua formatter
+				"eslint_d", -- ts/js linter
+				"shfmt",
+				"ruff",
+				"mypy",
+			},
+			-- auto-install configured formatters & linters (with null-ls)
+			automatic_installation = true,
+		})
+
+		local sources = {
+			diagnostics.checkmake,
+			formatting.prettier.with({ filetypes = { "html", "json", "yaml", "markdown" } }),
+			formatting.stylua,
+			formatting.shfmt.with({ args = { "-i", "4" } }),
+			formatting.terraform_fmt,
+			require("none-ls.formatting.ruff").with({ extra_args = { "--extend-select", "I" } }),
+			require("none-ls.formatting.ruff_format"),
+			diagnostics.mypy,
+		}
         vim.keymap.set("n", "<leader>fo", vim.lsp.buf.format, {})
-    end,
+
+		null_ls.setup({
+			-- debug = true, -- Enable debug mode. Inspect logs with :NullLsLog.
+			sources = sources,
+		})
+	end,
 }
