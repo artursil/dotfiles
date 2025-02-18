b5a3470a8b1936e178ea3b0539c6f3903c873ef4
artursil <artur.silicki@gmail.com>
2025-02-18 19:27:01 +0100

    tmuxifier

--- setup.sh
+++ setup.sh
@@ -31,6 +31,8 @@ fi
 
 source ~/.zshrc
 
+git clone https://github.com/jimeh/tmuxifier.git ~/.tmuxifier
+
 if ! command -v lazygit &> /dev/null; then
     LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
     cd ~/Downloads 
